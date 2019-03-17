74f01e4658a79bc004a759c7c5f5f4ec94660cde
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2019-03-16 19:41:57 -0700

    touch function to console.log complete

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
-import { Text, StyleSheet } from "react-native";
-import { connect } from 'react-redux';
+import { Text, StyleSheet, TouchableWithoutFeedback, View } from "react-native";
+import { connect } from "react-redux";
 import { CardSection } from "./common";
-import * as actions from '../actions';
+import * as actions from "../actions";
 
 class ListItem extends Component {
     render() {
         const { titleStyle } = styles;
-        console.log(this.props);
+        const { id, title } = this.props.library.item;
+        
         return (
-            <CardSection>
-                <Text style={titleStyle}>
-                    {this.props.library.item.title}
-                </Text>
-            </CardSection>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -25,4 +32,7 @@ const styles = StyleSheet.create({
     }
 });
 
-export default connect(null, actions)(ListItem);
+export default connect(
+    null,
+    actions
+)(ListItem);
