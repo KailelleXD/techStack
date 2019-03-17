2cbdcdd84dfbad4188e79486644a9d6bbd63d94c
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2019-03-16 17:41:28 -0700

    listview flatlist complete

---
+++ src/components/ListItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Text, StyleSheet } from "react-native";
+import { CardSection } from "./common/CardSection";
+
+export default class ListItem extends Component {
+    render() {
+        const { titleStyle } = styles;
+        return (
+            <CardSection>
+                <Text style={titleStyle}>
+                    {this.props.library.item.title}
+                </Text>
+            </CardSection>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+});
